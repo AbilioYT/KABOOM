c09bae453b93e3a8ae09bea3173e412f145cebd9
AbilioYT <153201131+AbilioYT@users.noreply.github.com>
2024-05-16 15:40:19 +0100

    Delete fundo.js

--- fundo.js
+++
@@ -1,7 +0,0 @@
-<script>
-var cor1 = document.getElementById("cor1");
-var cor2 = document.getElementById("cor2");
-var body = document.getElementById("fundo");
-function setGradient() {
- body.style.background = linear-gradient(to right " , " + cor1.value +" ," + cor2.value};
-</script>
