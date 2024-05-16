39b1a0ff72893a828e1ff3df82660bd63aeba66c
AbilioYT <153201131+AbilioYT@users.noreply.github.com>
2024-05-16 15:16:28 +0100

    Update fundo.js

--- fundo.js
+++ fundo.js
@@ -3,6 +3,6 @@ var cor1 = document.getElementById("cor1");
 var cor2 = document.getElementById("cor2");
 var body = document.getElementById("fundo");
 function setGradient() {
- body.style.background = "linear-gradient(to right, " + …
+ body.style.background = "linear-gradient(to right, " + cor1.value +" ," + cor2.value
 }
 </script>
